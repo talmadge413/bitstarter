297b3948ee9c1c7fc0d0816bf086b9869629c51f
Peter Talmadge <talmadge413@yahoo.com>
2013-07-08 02:57:49 +0000

    Troubleshoot web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buf = new Buffer(fs.readFileSync('./index.html'));
   //response.send(buf);
   //response.send(buf.toString('utf-8'));
