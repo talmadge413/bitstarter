ac93a7504eea9488a1d717d8ca5ec3191a8dd313
Peter Talmadge <talmadge413@yahoo.com>
2013-07-08 02:43:31 +0000

    Make web.js read index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = new Buffer(fs.readFileSync('index.html');
+  response.send(buf.toString('utf-8'));
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
